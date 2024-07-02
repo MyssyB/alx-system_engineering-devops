eef262813d8a3d2bb0638b05cf18ddedad084f61
MyssyB <beulahagiande@gmail.com>
2024-07-02 12:25:36 -0700

    correction

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,23 @@
 #This manifest installs a flask package using pip3
 
 $deps = ['python3', 'python3-pip']
-package { $deps:
+package {$deps:
 ensure => 'installed',
 }
-
-exec {'install flask':
+exec {'install_flask':
 command => '/usr/bin/pip3 install flask==2.1.0',
 }
+
+#class install_flask{
+#package {'python3-pip':
+#ensure => 'installed',
+#}
+
+#exec {'install_flask':
+#command => '/usr/bin/pip3 install flask==2.1.0',
+#unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+#require => Package['python3-pip']',
+#}
+#}
+
+#include install_flask
