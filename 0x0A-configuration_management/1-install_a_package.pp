2a282fe6fbbe5fadcfdc01fd84085d4eb55d0628
MyssyB <beulahagiande@gmail.com>
2024-07-02 09:37:17 -0700

    congif management tasks

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,10 @@
+#This manifest installs a flask package using pip3
+
+$deps = ['python3', 'python3-pip']
+package { $deps:
+ensure => 'installed',
+}
+
+exec {'install flask':
+command => '/usr/bin/pip3 install flask==2.1.0',
+}
