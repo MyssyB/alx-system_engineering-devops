0e46f7bdc9420d82dba382d899594f7c3632c8e4
MyssyB <beulahagiande@gmail.com>
2024-07-31 08:23:26 -0700

    api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Displays the employee ToDo list progress"""
+
+import requests
+import sys
+
+
+if __name__ == '__main__':
+
+    userId = sys.argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(userId))
+
+    EMPLOYEE_NAME = user.json().get('name')
+
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    NUMBER_OF_DONE_TASKS = 0
+    TOTAL_NUMBER_OF_TASKS = 0
+
+    for task in todos.json():
+        if task.get('userId') == int(userId):
+            TOTAL_NUMBER_OF_TASKS += 1
+            if task.get('completed'):
+                NUMBER_OF_DONE_TASKS += 1
+
+    print('Employee {} is done with tasks({}/{}):'
+            .format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
+
+    print('\n'.join(["\t " + task.get('title') for task in todos.json()
+        if task.get('userId') == int(userId) and task.get('completed')]))
