599191d25c21226751e917d022f190664dbee769
Myssyb <beulahagiande@gmail.com>
2024-07-31 20:46:27 +0100

    csv files

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+"""Extend python script to export data in the csv format"""
+
+import csv
+import requests
+import sys
+
+if __name__ == "__main__":
+    USER_ID = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(url + "users/{}".format(USER_ID)).json()
+    USERNAME  = user.get("USERNAME")
+    todos = requests.get(url + "todos", params={"userId": USER_ID}).json()
+
+    with open("USER_Ii.csv".format(USER_ID), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [USER_ID, USERNAME, t.get("TASK_COMPLETED_STATUS"), t.get("TASK_TITLE")]
+            ) for t in todos]
