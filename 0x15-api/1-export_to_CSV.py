22a2c59eaf6cf4c31531e6c5dc6ea36aa95aa197
MyssyB <beulahagiande@gmail.com>
2024-07-31 14:42:42 -0700

    write to a csv file

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -4,16 +4,23 @@
 import csv
 import requests
 import sys
+import json
+
 
 if __name__ == "__main__":
     USER_ID = sys.argv[1]
+
     url = "https://jsonplaceholder.typicode.com/"
+
     user = requests.get(url + "users/{}".format(USER_ID)).json()
-    USERNAME = user.get("USERNAME")
+
+    USERNAME = user.get("username")
+
     todos = requests.get(url + "todos", params={"userId": USER_ID}).json()
 
-    with open("USER_ID.csv".format(USER_ID), "w", newline="") as csvfile:
+    with open("{}.csv".format(USER_ID), "w", newline="") as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        writer.writerow([
-            [USER_ID, USERNAME, t.get("TASK_COMPLETED_STATUS"), t.get("TASK_TITLE")]
-            for t in todos])
+        writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+
+        for task in todos:
+            writer.writerow([USER_ID, USERNAME, task.get("completed"), task.get("title")])
