4950193d905f2194ac4187eede285ec0b0f1faba
MyssyB <beulahagiande@gmail.com>
2024-08-15 11:25:03 -0700

    api advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,8 +9,8 @@ def number_of_subscribers(subreddit):
 
     sub_info = requests.get("https://www.reddit.com/r/{}about.json"
                             .format(subreddit),
-                            headers={"User-Agent": "My-User_agent"},
-                            allow_redirects=Faslse)
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
     if sub_info.status_code >= 300:
         return 0
 
